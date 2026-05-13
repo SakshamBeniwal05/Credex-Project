56283e8b1d84f0e8985c1e53e28e3fbbf9450801
SakshamBeniwal05 <sakshambeniwal123@gmail.com>
2026-05-13 22:52:19 +0530

    ...

--- Backend/src/controller/static_controller.js
+++ Backend/src/controller/static_controller.js
@@ -336,7 +336,7 @@ export const plan_Data = async (req, res) => {
 
     try {
         const model = client.getGenerativeModel({
-            model: "gemini-2.5-flash",
+            model: "gemini-3.0-flash",
             systemInstruction: systemPrompt
         });
 
